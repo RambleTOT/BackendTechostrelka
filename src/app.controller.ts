05c9d0aafb6bca271962430e91c995a07db02300
RambleTOT <rambletot@gmail.com>
2024-03-22 23:46:42 +0500

    create first requests

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post, Req, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request, Response } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('/')
+  getHello(
+    @Req() req: any,
+    @Res() res: any
+  ) {
+    console.log(req.query)
+    return res.status(200).send(req.query)
   }
 }
